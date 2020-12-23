a29e7a4ef9846eece026108f69388627bd2b5fb3
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-23 18:16:33 +0100

    feat(on ne peut pas avoir deux joueurs avec le même pseudo dans une partie)

--- memory_backend/lib/memory_backend/Game.ex
+++ memory_backend/lib/memory_backend/Game.ex
@@ -1,3 +1,14 @@
 defmodule MemoryBackend.Game do
     defstruct identifiant: "000000", set: "", etat: "en_attente", joueurs: [], liste_carte: [], nombre_retournement: 0, nombre_tours: 0
+
+    def join(partie = %MemoryBackend.Game{}, joueur) do
+        liste_joueurs = partie.joueurs
+        case Enum.any?(liste_joueurs, &(&1 == joueur)) do
+            true -> 
+                {:error, "Joueur #{joueur} deja present"}
+            false -> 
+                partie = %MemoryBackend.Game{partie | joueurs: liste_joueurs ++ [joueur] }
+                {:ok, partie}
+        end   
+    end
 end
