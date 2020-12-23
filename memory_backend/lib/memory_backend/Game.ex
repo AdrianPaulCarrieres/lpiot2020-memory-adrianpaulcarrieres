19243eec87538a6aa9cf9504c5145c6b9669f8c3
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-23 18:26:51 +0100

    doc(ajout documentation module Game + doc fonction join) #1

--- memory_backend/lib/memory_backend/Game.ex
+++ memory_backend/lib/memory_backend/Game.ex
@@ -1,6 +1,22 @@
 defmodule MemoryBackend.Game do
-    defstruct identifiant: "000000", set: "", etat: "en_attente", joueurs: [], liste_carte: [], nombre_retournement: 0, nombre_tours: 0
+    @moduledoc """
+    Définition d'une partie.
+    """
 
+    defstruct identifiant: "000000", set: "", etat: "en_attente", joueurs: [], liste_carte: [], nombre_retournement: 0, nombre_tours: 0
+    
+    @doc """
+    Ajouter un joueur à une partie.
+
+    Le pseudo du joueur doit y être unique.
+
+    ## Examples
+
+        {:ok, partie} = MemoryBackend.Game.join(%MemoryBackend.Game{}, 'Adrian')
+        MemoryBackend.Game.join(partie, 'Adrian')                                
+        {:error, "Joueur Adrian deja present"}
+
+    """
     def join(partie = %MemoryBackend.Game{}, joueur) do
         liste_joueurs = partie.joueurs
         case Enum.any?(liste_joueurs, &(&1 == joueur)) do
