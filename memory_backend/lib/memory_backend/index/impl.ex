f8d7bc945fa46ee062cd47b20fae427bbfffe0a2
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-24 23:58:20 +0100

    feat(added implementation of creating a new game)

--- memory_backend/lib/memory_backend/index/impl.ex
+++ memory_backend/lib/memory_backend/index/impl.ex
@@ -1,1 +1,16 @@
+defmodule MemoryBackend.Index.Impl do
+  alias MemoryBackend.Game
 
+  @moduledoc """
+  Implements the logic needed to manage our games.
+  """
+
+  @doc """
+  Create the game with the given parameters.
+  Because the player creating it is the first one to join it we can avoid using the join function.
+  """
+  def create_game(id, deck = %MemoryBackend.Model.Deck{}, player) do
+    %Game{id: id, deck: deck, players: [player]}
+    |> Game.populate_cards_list()
+  end
+end
