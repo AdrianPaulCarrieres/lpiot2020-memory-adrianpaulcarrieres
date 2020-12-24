afccd3858c35eb1d080c49c3a25072ec056c19c1
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-24 11:44:42 +0100

    feat(added func to get cards or score associated to a theme)

--- memory_backend/lib/memory_backend/model/deck.ex
+++ memory_backend/lib/memory_backend/model/deck.ex
@@ -1,10 +1,23 @@
 defmodule MemoryBackend.Model.Deck do
     use Ecto.Schema
+    import Ecto.Query
 
     schema "deck" do
-        field :label, :string
+        field :theme, :string
 
         has_many :card, MemoryBackend.Model.Card
         has_many :score, MemoryBackend.Model.Score
+    end
+
+    def get_all_theme() do
+        MemoryBackend.Repo.all from d in MemoryBackend.Model.Deck
+    end
+
+    def get_associated_cards(theme) do
+        MemoryBackend.Repo.one from d in MemoryBackend.Model.Deck, where: d.label == ^theme, preload: [:card]
+    end
+
+    def get_associated_high_scores(theme) do
+        MemoryBackend.Repo.one from d in MemoryBackend.Model.Deck, where: d.label == ^theme, preload: [:score]
     end
 end
