c4bd45438304a6c66fda6f04638521c25950ce38
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-24 23:57:22 +0100

    feat(added API endpoints to our genserver)

--- memory_backend/lib/memory_backend/index.ex
+++ memory_backend/lib/memory_backend/index.ex
@@ -11,12 +11,26 @@ defmodule MemoryBackend.Index do
     GenServer.start_link(@server, opts, name: @server)
   end
 
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
   @doc """
   Create a new game and register it in our GenServer registry.
 
   Takes a player "host" pseudo and a deck to init the game.
   """
-  def create_game(deck = %MemoryBackend.Model.Deck{}, player) do
-    GenServer.call(@server, {:create_game, deck, player})
+  def create_game(id, deck = %MemoryBackend.Model.Deck{}, player) do
+    GenServer.call(@server, {:create_game, id, deck, player})
+  end
+
+  def find_game(id) do
+    GenServer.call(@server, {:find_game, id})
   end
 end
