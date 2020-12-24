fc94bdc5f96080e10158cc8fb76189b59a1bae4e
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-24 14:31:48 +0100

    test(test if player can be added to game stored in an agent) #2

---
+++ memory_backend/test/memory_backend/game_store_test.exs
@@ -0,0 +1,16 @@
+defmodule MemoryBackend.GameStoreTest do
+  use ExUnit.Case, async: true
+
+  test "add player to the game" do
+    {:ok, game_store} = MemoryBackend.GameStore.start_link([])
+
+    game = MemoryBackend.GameStore.get game_store
+    assert game == %MemoryBackend.Game{}
+
+    {:ok, game} = MemoryBackend.Game.join(game, "Adrian")
+
+    MemoryBackend.GameStore.set(game_store, game)
+
+    assert MemoryBackend.GameStore.get(game_store) == %MemoryBackend.Game{players: ["Adrian"]}
+  end
+end
