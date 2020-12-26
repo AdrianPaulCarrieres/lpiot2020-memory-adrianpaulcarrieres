ca5d4a27b430a76992df91df686548c949ac6f91
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-26 18:46:27 +0100

    feat(refactored the design to exchange messages only on a game channel. Scores will be broadcasted and handled per subtopic and filter)

--- memory_backend/lib/memory_backend_web/channels/game_channel.ex
+++ memory_backend/lib/memory_backend_web/channels/game_channel.ex
@@ -1,6 +1,13 @@
 defmodule MemoryBackendWeb.GameChannel do
   use MemoryBackendWeb, :channel
   alias MemoryBackend.Index
+  alias MemoryBackend.Model.Deck
+
+  def join("game:general", _payload, socket) do
+    decks = Deck.get_all_decks_with_high_scores()
+
+    {:ok, %{decks: decks}, socket}
+  end
 
   def join("game:" <> game_id, _params, socket) do
     player = socket.assigns.player
@@ -19,9 +26,21 @@ defmodule MemoryBackendWeb.GameChannel do
     end
   end
 
-  def handle_in("start_game", _payload, socket) do
-    "game:" <> game_id = socket.topic
+  def handle_in(
+        "create_game",
+        %{game_id: game_id, deck: deck = %Deck{}},
+        socket = %Phoenix.Socket{topic: "game:general"}
+      ) do
+    case Index.create_game(game_id, deck, socket.assigns.player) do
+      {:ok, game} ->
+        {:reply, {:ok, game}, socket}
 
+      {:error, msg} ->
+        {:reply, {:error, msg}, socket}
+    end
+  end
+
+  def handle_in("start_game", _payload, socket = %Phoenix.Socket{topic: "game:" <> game_id}) do
     case Index.start_game(game_id) do
       {:ok, game} ->
         broadcast!(socket, "start_game", %{game: game})
@@ -32,17 +51,18 @@ defmodule MemoryBackendWeb.GameChannel do
     end
   end
 
-  def handle_in("find_game", _payload, socket) do
-    "game:" <> game_id = socket.topic
-
+  def handle_in("find_game", _payload, socket = %Phoenix.Socket{topic: "game:" <> game_id}) do
     case Index.find_game(game_id) do
       {:ok, game} -> {:reply, {:ok, game}, socket}
       error_tuple -> {:reply, error_tuple, socket}
     end
   end
 
-  def handle_in("flip_card", {card_index, turn}, socket) do
-    "game:" <> game_id = socket.topic
+  def handle_in(
+        "flip_card",
+        {card_index, turn},
+        socket = %Phoenix.Socket{topic: "game:" <> game_id}
+      ) do
     active_player = socket.assigns.player
 
     case Index.flip_card(game_id, active_player, card_index, turn) do
--- memory_backend/lib/memory_backend_web/channels/user_socket.ex
+++ memory_backend/lib/memory_backend_web/channels/user_socket.ex
@@ -2,8 +2,7 @@ defmodule MemoryBackendWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  channel "game:*", MemoryBackendWeb.Game
-  channel "score:*", MemoryBackendWeb.ScoreChannel
+  channel "game:*", MemoryBackendWeb.GameChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
