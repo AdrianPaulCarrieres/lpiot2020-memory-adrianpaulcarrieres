8570eb4c5d6d69f82fb2acb90820e18b794615fb
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-26 23:47:17 +0100

    feat(added OS monitor to liveboard)

--- memory_backend/mix.exs
+++ memory_backend/mix.exs
@@ -20,7 +20,7 @@ defmodule MemoryBackend.MixProject do
   def application do
     [
       mod: {MemoryBackend.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
