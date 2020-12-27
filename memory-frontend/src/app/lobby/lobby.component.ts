7c9124b549d550d9d570ecdcf91a59f3eaaff77a
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-27 21:22:00 +0100

    formated file too

--- memory-frontend/src/app/lobby/lobby.component.ts
+++ memory-frontend/src/app/lobby/lobby.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ChannelService} from './../services/channel.service';
+import { ChannelService } from './../services/channel.service';
 
-import {Score} from './../models/score.model';
+import { Score } from './../models/score.model';
 
 @Component({
   selector: 'app-lobby',
@@ -9,7 +9,7 @@ import {Score} from './../models/score.model';
   styleUrls: ['./lobby.component.css']
 })
 export class LobbyComponent implements OnInit {
-  
+
   scores: Score[]
 
   constructor(private channelService: ChannelService) { }
@@ -21,12 +21,12 @@ export class LobbyComponent implements OnInit {
   joinLobby(): void {
     this.channelService.join_lobby()
       .subscribe({
-        next: function(data) {
+        next: function (data) {
           console.log("Score is " + data)
           this.scores = this.scores.append(data)
-        }, error: function(errorMessage) {
+        }, error: function (errorMessage) {
           console.log("Recieved the error with following message: " + errorMessage);
-        }, complete: function() {
+        }, complete: function () {
           console.log("Observable has completed Execution");
         }
       })
