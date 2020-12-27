90f03894967beffa163225dc65252c2b032efe56
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-27 21:02:01 +0100

    feat(decks list associated to the channels to populate the lobby page on join)

--- memory-frontend/src/app/services/channel.service.ts
+++ memory-frontend/src/app/services/channel.service.ts
@@ -6,7 +6,7 @@ import * as Phoenix from 'phoenix';
 
 
 import { Score } from '../models/score.model';
-import { Deck} from '../models/deck.model';
+import { Deck } from '../models/deck.model';
 
 @Injectable({
   providedIn: 'root'
@@ -15,42 +15,52 @@ export class ChannelService {
 
   public socket: any;
   public channel: any;
+  public decks: [Deck];
 
   constructor() {
     this.connect();
-   }
-
-  connect(): void{
-    this.socket = new Phoenix.Socket(environment.socket_endpoint + '/socket', {params: {player: "123"}});
-
-    this.socket.connect();
-    console.log(this.socket.isConnected())
-    this.join_lobby();
   }
 
-  join_lobby(): Observable<Score>{
-    return new Observable((observer: Observer<Score>) => {
-      if (!this.socket.isConnected()) {
-        this.channel = this.socket.channel('game:general');
-        this.channel.join()
-          .receive("ok", resp => {
-            console.log("Joined successfully", resp);
-          })
-          .receive("error", resp => {
-            console.log("Unable to join", resp);
-            return observer.error("unable to join");
-          })
-          .receive('timeout', () => {
-            console.log("timeout")
-            return observer.error("unable to join"); 
-          });
+  connect(): void {
+    this.socket = new Phoenix.Socket(environment.socket_endpoint + '/socket', { params: { player: "123" } });
 
-        this.channel.on("new_highscore", msg =>{
+    this.socket.connect();
+  }
+
+  join_lobby(): Observable<Score> {
+    return new Observable((observer: Observer<Score>) => {
+      this.channel = this.socket.channel('game:general');
+      this.channel.join()
+        .receive("ok", resp => {
+          console.log("Joined successfully");
+          console.table(resp.decks);
+          this.decks = this.parse_decks(resp.decks);
+        })
+        .receive("error", resp => {
+          console.log("Unable to join", resp);
+          return observer.error("unable to join");
+        })
+        .receive('timeout', () => {
+          console.log("timeout")
+          return observer.error("unable to join");
+        });
+      if (!this.socket.isConnected()) {
+
+
+        this.channel.on("new_highscore", msg => {
           let score = new Score(msg);
           return observer.next(score);
         });
       }
     });
+  }
+
+  parse_decks(array_to_parse: [string]): [Deck] {
+    var decks: [Deck] = [null]
+    for (var i = 0; i < array_to_parse.length; i++) {
+      decks.push(new Deck(array_to_parse[i]));
+    }
+    return decks;
   }
 
 }
