625d00b962b91a194aafc2dee418545bf05433e4
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-27 13:57:08 +0100

    feat(added a seeder module to load images in the db)

--- memory_backend/lib/memory_backend/database_seeder.ex
+++ memory_backend/lib/memory_backend/database_seeder.ex
@@ -1,7 +1,36 @@
 defmodule MemoryBackend.DatabaseSeeder do
   alias MemoryBackend.Model
 
-  def load_gems() do
+  def seed() do
+    load_gems()
+  end
+
+  defp load_gems() do
     theme = "Gems"
+    cards_names = ["A", "B", "C", "D", "E", "F", "G", "H"]
+
+    card_back = read_card_back(theme)
+
+    cards =
+      Enum.map(cards_names, fn card_name ->
+        image = read_card(theme, card_name)
+        %{image: image}
+      end)
+
+    players = Enum.map(1..4, fn _x -> %{player_name: "Your name belongs here"} end)
+
+    scores = Enum.map(1..10, fn _x -> %{score: 999, players: players} end)
+
+    attrs = %{theme: theme, card_back: card_back, cards: cards, scores: scores}
+
+    Model.create_deck(attrs)
+  end
+
+  defp read_card_back(theme) do
+    File.read!("#{theme}/card_back.PNG")
+  end
+
+  defp read_card(theme, card_name) do
+    File.read!("#{theme}/cards/#{card_name}.PNG")
   end
 end
