7ef7585233fbca5f61caf17807065a617911a19a
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-27 21:21:31 +0100

    feat(actually forgot the card model)

--- memory-frontend/src/app/models/card.model.ts
+++ memory-frontend/src/app/models/card.model.ts
@@ -1,17 +1,27 @@
 import { BaseModel } from './base.model';
 
 export interface CardInterface {
-    deck_id: string
     image: string
 }
 
 export class Card extends BaseModel implements CardInterface {
 
-    deck_id: string
     image: string
-    
+
 
     constructor(data?: any) {
         super(data);
+        if (data.image) {
+            this.image = data.image;
+        }
+    }
+
+    public static parse_image(array_to_parse: [string]): [Card] {
+        var cards: [Card] = [null]
+        for (var i = 0; i < array_to_parse.length; i++) {
+            cards.push(new Card(array_to_parse[i]));
+        }
+        cards.splice(0, 1)
+        return cards;
     }
 }
