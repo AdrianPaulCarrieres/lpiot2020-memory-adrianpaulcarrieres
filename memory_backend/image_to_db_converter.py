9d44e5a740f9f1edad48aaefcf2a66ee854154eb
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-27 12:25:26 +0100

    feat(creating an image to binary converter, it will put our images in the db)

---
+++ memory_backend/image_to_db_converter.py
@@ -0,0 +1,41 @@
+import psycopg2
+
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        blobData = file.read()
+    return blobData
+
+
+def connect(params):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            return conn
+
+
+connection = connect(
+    "dbname=memory_backend_dev user=postgres password=postgres")
