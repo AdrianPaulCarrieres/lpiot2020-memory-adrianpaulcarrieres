ed8ea201f34079b206e44b74fca708ed9eaec303
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-28 23:33:34 +0100

    feat(added animations to cards)

--- memory-frontend/src/app/game-card/game-card.component.ts
+++ memory-frontend/src/app/game-card/game-card.component.ts
@@ -1,11 +1,29 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Card, Deck } from '../models';
+
 
 
 @Component({
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
-  styleUrls: ['./game-card.component.css']
+  styleUrls: ['./game-card.component.css'],
+  animations: [
+    trigger('cardFlip', [
+      state('0', style({
+        transform: 'none',
+      })),
+      state('1', style({
+        transform: 'perspective(600px) rotateY(180deg)'
+      })),
+      transition('0 => 1', [
+        animate('400ms')
+      ]),
+      transition('1 => 0', [
+        animate('400ms')
+      ])
+    ])
+  ]
 })
 export class GameCardComponent implements OnInit {
 
