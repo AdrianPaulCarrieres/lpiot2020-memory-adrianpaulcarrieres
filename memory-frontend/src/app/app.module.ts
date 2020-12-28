4e235d73d9f521a2576824673ff882c0287f21d9
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2020-12-28 21:18:25 +0100

    feat(generated home component, will display a text field to get player name and launch socket connection)

--- memory-frontend/src/app/app.module.ts
+++ memory-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LobbyComponent } from './lobby/lobby.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LobbyComponent
+    LobbyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
